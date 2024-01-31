763552c2ebf87697312f5439b0b3b2e5aaf5611d
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-31 15:44:43 -0300

    set up game content

--- spaceinvaders/Game1.cs
+++ spaceinvaders/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Net.Mime;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,16 +11,29 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballSpeed;
+
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
+        _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
+        _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height -100;
+
+        _graphics.ApplyChanges();
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+        _graphics.PreferredBackBufferHeight / 2);
+        ballSpeed = 100f;
+
 
         base.Initialize();
     }
@@ -26,6 +41,8 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        ballTexture = Content.Load<Texture2D>("ship");
 
         // TODO: use this.Content to load your game content here
     }
@@ -44,6 +61,21 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(
+            ballTexture,
+            ballPosition,
+            null,
+            Color.White,
+            0f,
+            new Vector2(ballTexture.Width / 2, - _graphics.PreferredBackBufferHeight / 2 + ballTexture.Height + 50),
+            Vector2.One,
+            SpriteEffects.None,
+            0f
+        );
+
+        _spriteBatch.End();
         // TODO: Add your drawing code here
 
         base.Draw(gameTime);
