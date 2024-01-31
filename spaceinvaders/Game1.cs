d868e8270916dc6a7c5c9326435a15156f148953
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-31 17:11:24 -0300

    feat: player movment

--- spaceinvaders/Game1.cs
+++ spaceinvaders/Game1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Mime;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -11,9 +10,8 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    Texture2D ballTexture;
-    Vector2 ballPosition;
-    float ballSpeed;
+    Texture2D playerTexture;
+    Vector2 playerPosition;
 
 
     public Game1()
@@ -30,10 +28,8 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+        playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
         _graphics.PreferredBackBufferHeight / 2);
-        ballSpeed = 100f;
-
 
         base.Initialize();
     }
@@ -42,7 +38,7 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        ballTexture = Content.Load<Texture2D>("ship");
+        playerTexture = Content.Load<Texture2D>("ship");
 
         // TODO: use this.Content to load your game content here
     }
@@ -52,7 +48,14 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        var kstate = Keyboard.GetState();
+        int PLAYER_SPEED = 20;
+
+        bool rightLimit = _graphics.PreferredBackBufferWidth > playerPosition.X + playerTexture.Width / 2;
+        if (kstate.IsKeyDown(Keys.D) && rightLimit) playerPosition.X += PLAYER_SPEED;
+
+        bool leftLimit = playerTexture.Width / 2 < playerPosition.X;
+        if (kstate.IsKeyDown(Keys.A) && leftLimit) playerPosition.X -= PLAYER_SPEED;
 
         base.Update(gameTime);
     }
@@ -64,19 +67,19 @@ public class Game1 : Game
         _spriteBatch.Begin();
 
         _spriteBatch.Draw(
-            ballTexture,
-            ballPosition,
+            playerTexture,
+            playerPosition,
             null,
             Color.White,
             0f,
-            new Vector2(ballTexture.Width / 2, - _graphics.PreferredBackBufferHeight / 2 + ballTexture.Height + 50),
+            new Vector2(playerTexture.Width / 2, - _graphics.PreferredBackBufferHeight / 2 + playerTexture.Height + 50),
             Vector2.One,
             SpriteEffects.None,
             0f
         );
+        // TODO: Add you+r drawing code here
 
         _spriteBatch.End();
-        // TODO: Add your drawing code here
 
         base.Draw(gameTime);
     }
