e2e9e1fd24ec406a3de7405902b13d6e6fe955c3
willian <willian.cacoelho@gmail.com>
2024-02-07 20:32:44 -0300

    fix: collidion whe red ship enemy is going to left

--- spaceinvaders/src/model/aliens/RedEnemy.cs
+++ spaceinvaders/src/model/aliens/RedEnemy.cs
@@ -13,12 +13,9 @@ public class RedEnemy : IEnemyEntity
     private readonly SpriteBatch _spriteBatch;
     private readonly Texture2D _texture;
     private bool isDead = false;
-    private readonly float ALIEN_SPEED_Y = 0f;
-    private readonly float ALIEN_SPEED_X = 0f;
-    private readonly int changePositionDirectionDelay = 200;
-    private int changePositionDirectionCounter = 0;
-    private int isRightOrLeft;
+    private readonly float ALIEN_SPEED_X = 4f;
     private readonly Random random = new();
+    private readonly int isRightOrLeft;
 
 
     public RedEnemy(ContentManager contentManager, SpriteBatch spriteBatch, GraphicsDeviceManager graphics)
@@ -26,10 +23,12 @@ public class RedEnemy : IEnemyEntity
         Texture2D texture = contentManager.Load<Texture2D>("aliens/red-alien-ship");
         _graphics = graphics;
 
-        int heightTop = 0;
-        int randomWidthPosition = random.Next(-400, 400);
-        int widthCenter = _graphics.PreferredBackBufferWidth / 2 - texture.Width / 2;
-        Vector2 position = new(widthCenter, heightTop);
+        isRightOrLeft = random.Next(0, 2);
+
+        int height = 10;
+        int widthInitialLocation = isRightOrLeft == 0 ? -50 : _graphics.PreferredBackBufferWidth + 50;
+        int width = widthInitialLocation - texture.Width / 2;
+        Vector2 position = new(width, height);
         Bounds = new RectangleF(position, new Size2(texture.Width, texture.Height));
 
         _spriteBatch = spriteBatch;
@@ -43,58 +42,29 @@ public class RedEnemy : IEnemyEntity
 
     public void Update()
     {
-        bool isInitialMovement = InitialMovement();
-        if (isInitialMovement) return;
-        // Console.WriteLine(isDead);
+        Console.WriteLine(isDead);
         IsOutsideFromMap();
-        MovementDelay();
         Movement();
-    }
-
-    private bool InitialMovement() {
-        if (Bounds.Position.Y < _graphics.PreferredBackBufferHeight / 2) {
-            Bounds.Position += new Vector2(0, ALIEN_SPEED_Y);
-            return true;
-        }
-
-        return false;
     }
 
     private void IsOutsideFromMap() {
         bool rightLimit = _graphics.PreferredBackBufferWidth > Bounds.Position.X + _texture.Width / 2;
-        bool leftLimit = _texture.Width / 2 < Bounds.Position.X;
-        bool bottomLimit = Bounds.Position.Y  > _graphics.GraphicsDevice.Viewport.Height * 1.5;
-
-        if (!rightLimit || !leftLimit || bottomLimit) isDead = true;
-    }
-
-    private void MovementDelay() {
-        if (changePositionDirectionCounter >= changePositionDirectionDelay) {
-            isRightOrLeft = random.Next(0, 4);
-            changePositionDirectionCounter = 0;
-            return;
+        bool leftLimit = 0 + _texture.Width / 2 < Bounds.Position.X;
+        if (isRightOrLeft == 0 && !rightLimit || isRightOrLeft == 1 && !leftLimit)
+        {
+            isDead = true;
         }
-
-        changePositionDirectionCounter++;
     }
 
     private void Movement() {
         switch (isRightOrLeft) {
             case 0:
-                Vector2 right = new(ALIEN_SPEED_X, ALIEN_SPEED_Y);
-                Bounds.Position += right;
-                break;
-            case 1:
-                Vector2 left = new(-ALIEN_SPEED_X, ALIEN_SPEED_Y);
-                Bounds.Position += left;
-                break;
-            case 2:
-                Vector2 onceLeft = new(-ALIEN_SPEED_X, 0);
-                Bounds.Position += onceLeft;
-                break;
-            case 3:
                 Vector2 onceRight = new(ALIEN_SPEED_X, 0);
                 Bounds.Position += onceRight;
+                break;
+            case 1:
+                Vector2 onceLeft = new(-ALIEN_SPEED_X, 0);
+                Bounds.Position += onceLeft;
                 break;
         }
     }
