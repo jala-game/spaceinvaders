d606193f917fc919182043771ff6583d55ddd245
iure <iurelimasilva@gmail.com>
2024-02-20 00:01:11 -0300

    feat: moviment of menu itens with arrows

--- spaceinvaders/src/screen-logic/screens/MainScreen.cs
+++ spaceinvaders/src/screen-logic/screens/MainScreen.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 public class MainScreen(
     GraphicsDeviceManager graphics,
@@ -10,17 +13,27 @@ public class MainScreen(
 {
     private SpriteFont _gameFont;
     private SpriteFont _gameFontSmall;
+    private SpriteFont _gameDescription;
     private int _selectedOption = 1;
+    private float delayToPress = 10f;
     public override void Initialize() { }
 
     public override void LoadContent()
     {
         _gameFont = contentManager.Load<SpriteFont>("fonts/PixeloidMonoGameOver");
-        _gameFontSmall = contentManager.Load<SpriteFont>("fonts/PixeloidMono");
+        _gameFontSmall = contentManager.Load<SpriteFont>("fonts/PixeloidMonoMenu");
+        _gameDescription = contentManager.Load<SpriteFont>("fonts/PixeloidMono");
     }
 
     public override void Update(GameTime gameTime)
     {
+        var kstate = Keyboard.GetState();
+        
+        delayToPress--;
+        if (delayToPress > 0) return;
+        
+        ModifyMenuSelection(kstate);
+        
         
     }
 
@@ -28,9 +41,27 @@ public class MainScreen(
     {
         DrawTitle();
         DrawMenu();
+        DrawDescription();
+        DrawItemMenuActive();
         
     }
 
+    private void ModifyMenuSelection(KeyboardState kstate)
+    {
+        float resetDelay = 10;
+        if (kstate.IsKeyDown(Keys.Up) && _selectedOption > 1)
+        {
+            _selectedOption--;
+            delayToPress = resetDelay;
+        }
+        
+        if (kstate.IsKeyDown(Keys.Down) && _selectedOption < 3)
+        {
+            _selectedOption++;
+            delayToPress = resetDelay;
+        }
+    }
+    
     private void DrawTitle()
     {
         string text = "Space Invaders";
@@ -40,15 +71,45 @@ public class MainScreen(
 
     private void DrawMenu()
     {
-        string textItem1 = "Play Game";
-        float textWidthItem1 = _gameFontSmall.MeasureString(textItem1).X / 2;
-        spriteBatch.DrawString(_gameFontSmall, textItem1, new Vector2(graphics.PreferredBackBufferWidth / 2 - textWidthItem1 ,300), Color.White);
+        List<string> stringsMenu = new List<string>() {"  Play Game","  View Leaderboards","  Game Control" };
+        int baseY = 400;
         
-        string textItem2 = "> Play Game";
-        float textWidthItem2 = _gameFontSmall.MeasureString(textItem1).X / 2;
-        spriteBatch.DrawString(_gameFontSmall, textItem2, new Vector2(graphics.PreferredBackBufferWidth / 2 - textWidthItem2 ,360), Color.White);
+        stringsMenu.ForEach(e =>
+        {
+            DrawMenuItem(e, baseY);
+            baseY += 100;
+        });
     }
-    
+
+    private void DrawMenuItem(string text, int y)
+    {
+        string textBase = "  Play Game";
+        float textWidthItem1 = _gameFontSmall.MeasureString(textBase).X / 2 + 45;
+        spriteBatch.DrawString(_gameFontSmall, text, new Vector2(graphics.PreferredBackBufferWidth / 2 - textWidthItem1 ,y), Color.White);
+    }
+
+    private void DrawItemMenuActive()
+    {
+        switch (_selectedOption)
+        {
+            case 1:
+                DrawMenuItem("> Play Game", 400);
+                break;
+            case 2:
+                DrawMenuItem("> View Leaderboards", 500);
+                break;
+            case 3:
+                DrawMenuItem("> Game Control", 600);
+                break;
+        }
+    }
+
+    private void DrawDescription()
+    {
+        string text = "Use the arrow keys to move between menu items and press enter to select the option.";
+        float textWidth = _gameDescription.MeasureString(text).X / 2;
+        spriteBatch.DrawString(_gameDescription, text, new Vector2(100,graphics.PreferredBackBufferHeight - 100), Color.White);
+    }
     
 }
 
