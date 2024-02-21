e9791426b728e92e593b55c573ea381b02d59b81
willian <willian.cacoelho@gmail.com>
2024-02-21 12:34:10 -0300

    feat: when press done save in a localstorage file and redirect to main screen

--- spaceinvaders/src/utils/LocalStorage.cs
+++ spaceinvaders/src/utils/LocalStorage.cs
@@ -1,5 +1,6 @@
-using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -9,17 +10,31 @@ public class LocalStorage {
     public static void AddUser(User user) {
         JObject data = JsonConvert.DeserializeObject<JObject>(jsonString);
 
-        JArray users = (JArray)data["users"];
-
         JObject newUser = new()
         {
             ["name"] = user.Name,
             ["score"] = user.Score
         };
 
-        users.Add(newUser);
+        data.Add(newUser);
 
-        string newJsonString = JsonConvert.SerializeObject(users, Formatting.Indented);
+        string newJsonString = JsonConvert.SerializeObject(data, Formatting.Indented);
         File.WriteAllText("localstorage/data.json", newJsonString);
+    }
+
+    public static List<User> GetUsersPaginator(int quantity, int page=0) {
+        JObject data = JsonConvert.DeserializeObject<JObject>(jsonString);
+
+        List<User> usersList = [];
+
+        int INITIAL_VALUE = page * quantity;
+
+        for (int i = INITIAL_VALUE; i < INITIAL_VALUE * 2; i++)
+        {
+            User user = data[i].ToObject<User>();
+            usersList.Add(user);
+        }
+
+        return usersList;
     }
 }
