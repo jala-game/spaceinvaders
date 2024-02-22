7f937eed623c3fd72219a02faa0a750094081920
iure <iurelimasilva@gmail.com>
2024-02-22 20:34:53 -0300

    feat: add logic for inicialize leaderboards screen and create this class

---
+++ spaceinvaders/src/screen-logic/screens/LeaderBoardsScreen.cs
@@ -0,0 +1,21 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace spaceinvaders.screen_logic.screens;
+
+public class LeaderBoardsScreen(
+    Game game,
+    GraphicsDeviceManager graphics,
+    ContentManager contentManager,
+    SpriteBatch spriteBatch
+) : GameScreenModel
+{
+    public override void Initialize() { }
+    
+    public override void LoadContent() { }
+    
+    public override void Update(GameTime gameTime) { }
+    
+    public override void Draw(GameTime gameTime) { }
+}
