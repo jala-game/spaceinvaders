1f9ad940bf955da5936c55a9bf63fb0446db8e9e
iure <iurelimasilva@gmail.com>
2024-02-25 13:16:43 -0300

    feat: add logic for draw titles

---
+++ spaceinvaders/src/enums/EGameControlOptions.cs
@@ -0,0 +1,8 @@
+namespace spaceinvaders.model.barricades;
+
+public enum EGameControlOptions
+{
+    LeaveGame = 1,
+    LeftArrow = 2,
+    RightArrow = 3
+}
--- spaceinvaders/src/screen-logic/screens/GameControlScreen.cs
+++ spaceinvaders/src/screen-logic/screens/GameControlScreen.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using spaceinvaders.model.barricades;
 
 namespace spaceinvaders.screen_logic.screens;
 
@@ -10,4 +11,49 @@ public class GameControlScreen(
     ContentManager contentManager,
     SpriteBatch spriteBatch) : GameScreenModel
 {
+    private SpriteFont _title = game.Content.Load<SpriteFont>("fonts/PixeloidMonoGameOver");
+    private SpriteFont _genericFont = game.Content.Load<SpriteFont>("fonts/PixeloidMonoMenu");
+    private int _page = 0;
+    private EMenuOptionsLeaderBoards _chooseMenu = EMenuOptionsLeaderBoards.RightArrow;
+    private float delayToPress = 10f;
+    
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public override void LoadContent()
+    {
+        base.LoadContent();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        base.Draw(gameTime);
+    }
+
+    private void DrawControls()
+    {
+        DrawTitle("Controls for Ship", null);
+    }
+
+    private void DrawGameScore()
+    {
+        DrawTitle("Game Score", Color.Gold);
+    }
+    
+
+    private void DrawTitle(string text, Color? color)
+    {
+        float textWidth = _title.MeasureString(text).X / 2;
+        spriteBatch.DrawString(_title, text, new Vector2(graphics.PreferredBackBufferWidth / 2 - textWidth, 50),
+            color ?? Color.White);
+    }
+    
+    
 }
