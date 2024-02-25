65d63aaa393e056daba84dbd60acd6a8f2b9d872
iure <iurelimasilva@gmail.com>
2024-02-25 13:36:18 -0300

    feat: add logic for draw controls

--- spaceinvaders/src/screen-logic/screens/GameControlScreen.cs
+++ spaceinvaders/src/screen-logic/screens/GameControlScreen.cs
@@ -34,20 +34,49 @@ public class GameControlScreen(
 
     public override void Draw(GameTime gameTime)
     {
+        switch (_page)
+        {
+            case 0:
+                DrawControls();
+                break;
+            case 1:
+                DrawGameScore();
+                break;
+        }
         base.Draw(gameTime);
     }
 
     private void DrawControls()
     {
         DrawTitle("Controls for Ship", null);
+        DrawKeyControls();
     }
 
     private void DrawGameScore()
     {
         DrawTitle("Game Score", Color.Gold);
+        DrawEnemyScore();
+    }
+
+    private void DrawKeyControls()
+    {
+
+        string[] controls = new[] {"Move to the left - Left arrow or A Key",
+            "Move to the right - Right arrow or D Key","Shoot - Space Key" };
+        int positionY = 300;
+
+        for (int i = 0; i < controls.Length; i++)
+        {
+            spriteBatch.DrawString(_genericFont, controls[i], new Vector2(300,positionY), Color.White);
+            positionY += 100;
+        }
+        
+    }
+    private void DrawEnemyScore()
+    {
+        
     }
     
-
     private void DrawTitle(string text, Color? color)
     {
         float textWidth = _title.MeasureString(text).X / 2;
