66dc5e9b7c23c1fb7af17da806bb76b2a243599b
willian <willian.cacoelho@gmail.com>
2024-02-25 11:28:28 -0300

    feat: go to LoadScreen if esc is pressed

--- spaceinvaders/src/Game1.cs
+++ spaceinvaders/src/Game1.cs
@@ -48,7 +48,7 @@ public class Game1 : Game
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+            LoadScreenManager();
 
         ScreenManager.Update(gameTime);
 
