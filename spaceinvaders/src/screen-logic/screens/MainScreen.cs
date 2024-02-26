64aea6b6e7407e983b1bebb2b870e35598a2da75
willian <willian.cacoelho@gmail.com>
2024-02-26 11:39:31 -0300

    feat: add animation and yellowspiral

--- spaceinvaders/src/screen-logic/screens/MainScreen.cs
+++ spaceinvaders/src/screen-logic/screens/MainScreen.cs
@@ -21,6 +21,7 @@ public class MainScreen(
     private SpriteFont _gameFontSmall = game.Content.Load<SpriteFont>("fonts/PixeloidMonoMenu");
     private SpriteFont _gameDescription = game.Content.Load<SpriteFont>("fonts/PixeloidMono");
     private EMenuScreenOptions _selectedOption = EMenuScreenOptions.Play;
+    private SpiralYellow spiralYellowAnimation = new SpiralYellow(spriteBatch, contentManager, new Vector2(20, 20));
     private float delayToPress = 10f;
     public override void Initialize() { }
 
@@ -39,6 +40,7 @@ public class MainScreen(
 
         ModifyMenuSelection(kstate);
         SendMenuOption(kstate);
+        spiralYellowAnimation.Update(gameTime);
 
     }
 
@@ -48,6 +50,7 @@ public class MainScreen(
         DrawMenu();
         DrawDescription();
         DrawItemMenuActive();
+        spiralYellowAnimation.Draw();
     }
 
     private void ModifyMenuSelection(KeyboardState kstate)
